0e821e7f09507e97c03dbf5f6cbe0d47c3164dbe
Michał Gajda <michalgajda88@gmail.com>
2022-07-10 16:19:38 +0200

    Chromedriver updated FirstWaits modified

--- src/test/java/Waits/FirstWaits.java
+++ src/test/java/Waits/FirstWaits.java
@@ -1,6 +1,7 @@
 package Waits;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -15,30 +16,42 @@ import java.time.Duration;
 public class FirstWaits {
     WebDriver driver;
     WebDriverWait wait;
+    String expectedPrice,
+            currentPrice;
 
     @BeforeEach
-    public void driverSetup(){
+    public void driverSetup() {
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(100000), Duration.ofSeconds(5));
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://fakestore.testelka.pl/");
     }
 
     @AfterEach
-    public void driverQuit(){
+    public void driverQuit() {
         driver.quit();
     }
 
     @Test
-    public void waitTest(){
+    public void waitTest() {
         driver.findElement(By.cssSelector("a[class='woocommerce-store-notice__dismiss-link']")).click();
-        WebElement addToCartButton = driver.findElement(By.cssSelector("section[class='storefront-product-section storefront-recent-products'] a[data-product_id='391']"));
-        wait.until(ExpectedConditions.visibilityOf(addToCartButton)).click();
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("a[class='added_to_cart wc-forward']")))).click();
 
-        WebElement inputQuantity = driver.findElement(By.cssSelector("input[class='input-text qty text']"));
-        wait.until(ExpectedConditions.visibilityOf(inputQuantity)).sendKeys("2");
+        driver.findElement(By.cssSelector("section[class*='recent'] li[class*='post-391']  a[class*='loop']")).click();
+        driver.findElement(By.cssSelector("button[class='single_add_to_cart_button button alt']")).click();
+
+        driver.findElement(By.cssSelector("div[role='alert'] a[class='button wc-forward']")).click();
+        WebElement inputQuantity = driver.findElement(By.cssSelector("div[class='quantity'] input[class*='input-text']"));
+        inputQuantity.clear();
+        inputQuantity.sendKeys("2");
+        driver.findElement(By.cssSelector("button[name='update_cart']")).click();
+
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("div[class*='blockUI']"), 0));
+
+        expectedPrice = "6 400,00" + " zł";
+        currentPrice = driver.findElement(By.cssSelector("tr[class='order-total'] strong")).getText();
+
+        Assertions.assertEquals(expectedPrice, currentPrice, "Total price: " + currentPrice + " is different thann expected: "
+                + expectedPrice);
     }
 }
