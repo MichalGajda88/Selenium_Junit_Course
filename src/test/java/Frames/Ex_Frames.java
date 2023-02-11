a6eb894896b2088070f1c701a16044b852245567
Michał Gajda <michalgajda88@gmail.com>
2023-02-11 23:46:56 +0100

    added methods in Ex_Frames class

--- src/test/java/Frames/Ex_Frames.java
+++ src/test/java/Frames/Ex_Frames.java
@@ -29,17 +29,47 @@ public class Ex_Frames {
     public void driverQuit() {
         driver.quit();
     }
+    //1. Potwierdź, że pierwszy przycisk „Strona główna” jest nieaktywny.
 
     @Test
-    public void frameContentTest() {
+    public void mainPageButtonIsDisabled() {
         driver.switchTo().frame("main-frame");
-        //1. Potwierdź, że pierwszy przycisk „Strona główna” jest nieaktywny.
         WebElement mainPageButton = driver.findElement(By.cssSelector("input[name='main-page']"));
         Assertions.assertFalse(mainPageButton.isEnabled(), "State of main page button is different than expected one");
+    }
 
-        //2. Potwierdź, że obrazek kieruje do strony głównej (sprawdź bez klikania w element).
+    //2. Potwierdź, że obrazek kieruje do strony głównej (sprawdź bez klikania w element).
+    @Test
+    public void imageRedirectToMainPage() {
+        driver.switchTo().frame("main-frame");
+        WebElement frame = driver.findElement(By.cssSelector("iframe[name='image']"));
         driver.switchTo().frame("image");
         String mainPageAddress = "https://fakestore.testelka.pl/";
-        WebElement mainPageImage = driver.findElement(By.cssSelector("img[alt='Wakacje']"));
+        WebElement mainPageImageLink = driver.findElement(By.xpath(".//img[@alt='Wakacje']/.."));
+        Assertions.assertEquals(mainPageAddress, mainPageImageLink.getAttribute("href"),
+                "Link address is different than expected one.");
+    }
+
+    @Test
+    public void mainPageButtonIsEnabled() {
+        driver.switchTo().frame("main-frame");
+        driver.switchTo().frame("image");
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[loading='lazy']")));
+        WebElement mainPageButton = driver.findElement(By.xpath(".//a[text()='>> Strona główna' and @class='button']"));
+        Assertions.assertTrue(mainPageButton.isEnabled(), "Main page button is inactive");
+    }
+    //4. Kliknij w przycisk „Wspinaczka” (po wcześniejszym kliknięciu w przycisk „>>Strona główna”)  i potwierdź, że po
+    // przejściu na stronę widoczne jest logo (w tej ramce).
+    @Test
+    public void logoIsDisplayed(){
+        driver.switchTo().frame("main-frame");
+        driver.switchTo().frame("image");
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[loading='lazy']")));
+        WebElement mainPageButton = driver.findElement(By.xpath(".//a[text()='>> Strona główna' and @class='button']"));
+        mainPageButton.click();
+        driver.switchTo().frame("main-frame");
+        WebElement climbingButton = driver.findElement(By.cssSelector("a[value='wspinaczka']"));
+        climbingButton.click();
+        WebElement logo = driver.findElement(By.xpath(".//a[@class='custom-logo-link']//img"));
     }
 }
