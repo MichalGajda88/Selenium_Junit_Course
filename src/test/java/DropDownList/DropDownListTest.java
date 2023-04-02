7b46a6136f73e400a8eeca6676e834ceac1a97b9
Michał Gajda <michalgajda88@gmail.com>
2023-04-02 12:33:14 +0200

    added test method in DropDownListTest class updated Readme

--- src/test/java/DropDownList/DropDownListTest.java
+++ src/test/java/DropDownList/DropDownListTest.java
@@ -25,12 +25,6 @@ public class DropDownListTest {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.manage().window().maximize();
-        driver.navigate().to("https://www.allegro.pl/");
-        WebElement alert = driver.findElement(By.cssSelector("div[role='alertdialog']"));
-        Cookie permission = new Cookie("gdpr_permission_given", "1");
-        driver.manage().addCookie(permission);
-        driver.navigate().refresh();
-        wait.until(ExpectedConditions.invisibilityOf(alert));
     }
 
     @AfterEach
@@ -40,6 +34,13 @@ public class DropDownListTest {
 
     @Test
     public void dropDownList() {
+        driver.navigate().to("https://www.allegro.pl/");
+        WebElement alert = driver.findElement(By.cssSelector("div[role='alertdialog']"));
+        Cookie permission = new Cookie("gdpr_permission_given", "1");
+        driver.manage().addCookie(permission);
+        driver.navigate().refresh();
+        wait.until(ExpectedConditions.invisibilityOf(alert));
+
         WebElement productCategoriesList = driver.findElement(By.cssSelector("select[data-role='filters-dropdown-toggle']"));
         Select productCategory = new Select(productCategoriesList);
         productCategory.selectByIndex(3);
@@ -49,5 +50,25 @@ public class DropDownListTest {
         List<WebElement> options = productCategory.getOptions();
         List<WebElement> allSelectedOptions = productCategory.getAllSelectedOptions();
         WebElement firstSelectedElement = productCategory.getFirstSelectedOption();
+    }
+
+    @Test
+    public void multiSelectDropDownList() {
+        driver.navigate().to("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple");
+        WebElement acceptButton = driver.findElement(By.cssSelector("div[id='accept-choices']"));
+        acceptButton.click();
+        wait.until(ExpectedConditions.invisibilityOf(acceptButton));
+
+        WebElement iframe = driver.findElement(By.cssSelector("iframe[id='iframeResult']"));
+        driver.switchTo().frame(iframe);
+        WebElement carList = driver.findElement(By.cssSelector("select[id='cars']"));
+        Select carSelect = new Select(carList);
+        carSelect.selectByValue("volvo");
+        carSelect.selectByValue("saab");
+        carSelect.deselectByIndex(1);
+        Boolean isMultiple = carSelect.isMultiple();
+        carSelect.selectByIndex(2);
+        carSelect.selectByIndex(3);
+        carSelect.deselectAll();
     }
 }
